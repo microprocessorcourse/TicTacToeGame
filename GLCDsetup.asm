8b9c70da6087ccc092473262139407611217d05c
j1bul <j1bul@DESKTOP-1KHON01.home>
2019-02-28 10:14:35 +0000

    added more comments changed loop labels

--- GLCDsetup.asm
+++ GLCDsetup.asm
@@ -16,17 +16,17 @@ Y_counter     res 1   ; counter for Y address
 Page_counter  res 1   ; counter for pages
 line_setter   res 1   ; counter for lines 
 y_address     res 1   ; reserve 1 byte for y_address input from keypad
-Xshape_counter res 1  ; reserve 1 byte 
-Xshape_drawer res 1
-Xshape_page res 1
-O_counter  res 1
-O_counter2 res 1
-Y_address res 1
-Page_address res 1
-row_input    res 1
+Xshape_counter res 1  ;  for X character counter
+Xshape_drawer res 1   ;  for writing x lines
+Xshape_page res 1     ;  for switching pages while drawing lines
+O_counter  res 1      ;  for O character counter
+O_counter2 res 1      ;  for 2nd half of O shape on next page
+Y_address res 1       ;  reserve memory for deciding which box to put an X or O, based on keypad press, coupled with page_address
+Page_address res 1    
+row_input    res 1    ; keypad inputs
 col_input    res 1
-combo      res 1
-    constant cs1=0 ; set names for control pins
+combo      res 1      ; row_input inclusive OR with col_input stored here
+    constant cs1=0 ; set names for control pins on PORTB and for certain instruction values
     constant cs2=1
     constant RS=2
     constant RW=3
@@ -41,22 +41,22 @@ combo      res 1
 GLCD code
 
 
-GLCD_init
+GLCD_init ; need to turn off GLCD and initialize by setting display start 0xC0 and page and Y address start then turn on again
     clrf LATB
-    clrf TRISD
+    clrf TRISD ; sets PORTB and PORTD as output
     clrf TRISB
-    bsf LATB, RST ; reset button on when low
-    bcf LATB, cs1 ; cs1 and cs2 on when low
+    bsf LATB, RST ; reset is ON when reset switch is low, so keep high to display GLCD
+    bcf LATB, cs1 ; cs1 and cs2 on when low, determines left and right half of display
     bcf LATB, cs2 
     movlw 20 ; 20ms delay after latch
     call LCD_delay_ms
-    movlw DISP_OFF ; must turn off display to initialize and then turn on again
+    movlw DISP_OFF ; init starts here
     call GLCD_Cmdwrite
-    movlw 0x40    ; set x address 0-63 is page 1
+    movlw Y_start    ; set Y address starting point, horizontal
     call GLCD_Cmdwrite
-    movlw 0xB8 ; sets line   line 0 in this case with B8 up to BF and each line has 8 pixels
+    movlw Page0 ; sets line   line 0 in this case with B8 up to BF and each line has 8 pixels
     call GLCD_Cmdwrite
-    movlw 0xC0 ; set starting pixel of code vertically
+    movlw 0xC0 ; set starting page of display, this is not changed once set as it shifts entire display
     call GLCD_Cmdwrite
     movlw DISP_ON
     call GLCD_Cmdwrite
@@ -64,120 +64,111 @@ GLCD_init
     call LCD_delay_ms
     return 
     
-GLCD_test
-    movlw 0x48
-    call GLCD_Cmdwrite
-    movlw 0xC0
-    call GLCD_Datawrite
-    return
-    
-GLCD_Cmdwrite
-    movwf PORTD
+GLCD_Cmdwrite ; write instructions to GLCD
+    movwf PORTD ; PORTD is data bus
     bcf LATB, RS ; RS low for command write
     bcf LATB, RW ; RW low for write
-    call LCD_Enable
+    call LCD_Enable 
     return  
     
-GLCD_Datawrite
+GLCD_Datawrite; write data to GLCD, 1 byte is a vertical row of 8 pixels at given Y and page
     movwf PORTD
     bsf LATB, RS ; RS high for data write
     bcf LATB, RW ; RW low for write
     call LCD_Enable
     return
     
-GLCD_clear
-	      movlw 0x08
+GLCD_clear; clear entire display
+	      movlw 0x08; 8 vertical pages in total with a byte/page/Yaddress
               movwf Page_counter
-Page_clear    movlw 0x40
+Page_clear    movlw Y_start
 	      call GLCD_Cmdwrite
-	      movlw 0x40
 	      movwf Y_counter
 y_loop        movf Page_counter, W
-	      addlw 0xB8
+	      addlw Page0; add 0xB8 to 0x08, start at last page clear bottom to top
 	      call GLCD_Cmdwrite
-	      movlw 0x00
-	      call GLCD_Datawrite
-	      decfsz Y_counter
+	      movlw 0x00; clear
+	      call GLCD_Datawrite; Y_address increments automatically here
+	      decfsz Y_counter; go through 0-63 Y addresses horizontally
               bra y_loop
-              decfsz Page_counter
+              decfsz Page_counter; once cleared page via Y addresses move up to next page
 	      bra Page_clear
 	      return
 
 	      
-GLCD_fill
+GLCD_fill; idnetical to GLCD_clear with 0xFF to fill
 	      movlw 0x08
               movwf Page_counter
-Page_fill    movlw 0x40
+Page_fill    movlw Y_start
 	      call GLCD_Cmdwrite
-	      movlw 0x40
 	      movwf Y_counter
-y_loopp       movf Page_counter, W
-	      addlw 0xB8
+y_loop_fill   movf Page_counter, W
+	      addlw Page0
 	      call GLCD_Cmdwrite
 	      movlw 0xFF; fills display
 	      call GLCD_Datawrite
 	      decfsz Y_counter
-              bra y_loopp
+              bra y_loop_fill
               decfsz Page_counter
 	      bra Page_fill
 	      return
 	      
 GLCD_set_horizontal
 	call GLCD_clear
-	movlw 0x40; start position
+	movlw Y_start; start position of line
 	call GLCD_Cmdwrite
 	movlw 0xBA; set to page 2
 	call GLCD_Cmdwrite
-	movlw 0x40; not 3F but need 0x40 or wont set last y adress
+	movlw 0x40; not 3F but need 0x40 to set last y adress
 	movwf Y_counter
-rerun	movlw 0x08; value for 0b00010000 on page 2
+rerun	movlw 0x08; 0b00010000 on page 2 to draw horizontal line
+	call GLCD_Datawrite; 
+	decfsz Y_counter
+	bra rerun; Y address incrementing alone after data write instruction 
+	movlw Y_start; second horizontal line on pg 5
+	call GLCD_Cmdwrite
+	movlw 0xBD; page 5
+	call GLCD_Cmdwrite
+	movlw 0x40; need 0x40 not 3F to set last y adress
+	movwf Y_counter
+rerun2	movlw 0x20 ; 0b00100000 on page 5 incremented across Y addresses
 	call GLCD_Datawrite
 	decfsz Y_counter
-	bra rerun; rerun with Y incrementing alone after data instruction 
-	movlw 0x40
-	call GLCD_Cmdwrite
-	movlw 0xBD; second horizontal line on page 5 now
-	call GLCD_Cmdwrite
-	movlw 0x40; not 3F but need 0x40 or wont set last y adress
-	movwf Y_counter
-rerunn	movlw 0x20 ; value for 0b00100000 on page 5
-	call GLCD_Datawrite
-	decfsz Y_counter
-	bra rerunn
+	bra rerun2
 GLCD_set_vertical
-	bsf LATB, cs2
-	movlw 0x65; set Y address for vertical line at 0x60
+	bsf LATB, cs2; turns off right screen, write to left only
+	movlw 0x65; set Y address for line1
 	call GLCD_Cmdwrite
 	bcf LATB, cs2
-	bsf LATB, cs1
-	movlw 0x55
+	bsf LATB, cs1; turn off left screen
+	movlw 0x55; set Y address for line2
 	call GLCD_Cmdwrite
 	bcf LATB, cs1
-	movlw 0xB8
+	movlw Page0; page0 init 
 	call GLCD_Cmdwrite
 	movlw 0xFF
 	call GLCD_Datawrite
-	movlw 0x07
+	movlw 0x07; rest of pages looped
 	movwf Page_counter
-hello	bsf LATB, cs2
-	movlw 0x65; set Y address for vertical line at 0x60
+rerun_v	bsf LATB, cs2
+	movlw 0x65; loops to keep Y_address from incrementing
 	call GLCD_Cmdwrite
 	bcf LATB, cs2
 	bsf LATB, cs1
 	movlw 0x55
 	call GLCD_Cmdwrite
 	bcf LATB, cs1
-	movf Page_counter, W ; unlike Y page wont increment so must use counter
-	addlw 0xB8; start at page 7 ie address 0xBF
+	movf Page_counter, W ; unlike Y, Page wont increment so must use counter
+	addlw Page0; start at last page and fill up 
 	call GLCD_Cmdwrite
 	movlw 0xFF
 	call GLCD_Datawrite
 	decfsz Page_counter
-	bra hello
+	bra rerun_v
 	return
 X_char
 line1	bsf LATB, cs2; turn off right scren to only draw X in first box
-	movf Y_address, W;  this is halfway point in 1st box to make x appear in middle of box obtained from vertical line Y address
+	movf Y_address, W; input from keypad sets Y_address and Page_address
 	call GLCD_Cmdwrite
 	movff Page_address, Xshape_page  ; do this on page 0
 pagelop	movf Xshape_page, W
@@ -292,7 +283,7 @@ test1; this is the correct way, registers f and W have to make sense create var.
     call X_char
 back return
 	
-LCD_Enable	    ; pulse enable bit LCD_E for 500ns
+LCD_Enable	    ; pulse enable bit EN, high to low is when write to GLCD
 	nop
 	nop
 	nop
@@ -310,7 +301,7 @@ LCD_Enable	    ; pulse enable bit LCD_E for 500ns
 	nop
 	nop
 	nop
-	bcf	    LATB, EN	    ; Writes data to LCD   high to low transitions
+	bcf	    LATB, EN	    ; Writes data to LCD, high to low transitions
 	movlw .1
 	call LCD_delay_ms
 	return
